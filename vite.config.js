51f97588f8b5ebfa6e547601d186c6bc4fd5699f
geummi0520 <rmaal520@gmail.com>
2025-11-19 08:37:48 +0900

    제발배포#2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@
 import { defineConfig } from "./node_modules/vite/dist/node/index";
 
 export default defineConfig({
-  base: '/repo-name/',  // 자신의 GitHub 저장소 이름으로 변경
+  base: '/Intothe-Forest/',  // 자신의 GitHub 저장소 이름으로 변경
   // 그 외 설정
 });
